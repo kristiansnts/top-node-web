ef6545316f2d1bc761f36f8c0bcc2e60dae716eb
kristiansnts <epafroditus.kristian@gmail.com>
2024-05-17 16:23:07 +0700

    fix

--- index.js
+++ index.js
@@ -1,62 +1,21 @@
-const http = require('http')
-const fs = require('fs').promises
+const http = require('http');
+const fs = require('fs');
 
-const host = 'localhost'
-const port = 8080
-
-let indexFile
-let aboutFile
-let contactFile
-let errFile
-
-const requestListener = (req, res) => {
+http.createServer((req, res) => {
     let url = req.url
-    if(url === '/'){
-        res.setHeader("Content-Type", "text/html")
-        res.writeHead(200)
-        res.end(indexFile)
-    } else if (url === '/about'){
-        res.setHeader("Content-Type", "text/html")
-        res.writeHead(200)
-        res.end(aboutFile)
-    } else if (url === '/contact-me') {
-        res.setHeader("Content-Type", "text/html")
-        res.writeHead(200)
-        res.end(contactFile)
+    if(url == '/') {
+        res.writeHead(200, {"Content-Type": "text/html"})
+        fs.createReadStream('index.html').pipe(res)
+    } else if (url == '/about'){
+        res.writeHead(200, {"Content-Type": "text/html"})
+        fs.createReadStream('about.html').pipe(res)
+    } else if (url == '/contact-me'){
+        res.writeHead(200, {"Content-Type": "text/html"})
+        fs.createReadStream('contact-me.html').pipe(res)
     } else {
-        res.setHeader("Content-Type", "text/html")
-        res.writeHead(200)
-        res.end(errFile)
+        res.writeHead(404, {"Content-Type": "text/html"})
+        fs.createReadStream('404.html').pipe(res)
     }
-}
-
-const server = http.createServer(requestListener)
-
-
-
-const startServer = (link) => {
-    fs.readFile(link)
-    .then(contents => {
-        if(link === 'index.html'){
-            indexFile = contents
-        } else if (link === 'about.html'){
-            aboutFile = contents
-        } else if (link === 'contact-me.html'){
-            contactFile = contents
-        } else {
-            errFile = contents
-        }
-        server.listen(port, host, () => {
-            console.log(`Server is running on http://${host}:${port}`)
-        })
-    })
-    .catch(err => {
-        console.error(`Could not read ${link} file: ${err}`);
-        process.exit(1);
-    })
-}
-
-startServer('index.html')
-startServer('about.html')
-startServer('contact-me.html')
-startServer('404.html')
+}).listen(8080, () => {
+    console.log(`Server running on port : 8080`)
+})
